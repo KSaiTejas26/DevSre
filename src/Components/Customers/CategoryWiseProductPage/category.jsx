0318c58adabdd2e54a1a58f7bfe92c2dd99cd42b
Shishir <shishir.lakshmivenkatashishir@realpage.com>
2024-06-10 17:06:36 +0530

    api commit

--- src/Components/Customers/CategoryWiseProductPage/category.jsx
+++ src/Components/Customers/CategoryWiseProductPage/category.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import Header from '../Navbar'
 import RightView from './RightView'
 import Footer from "../../Footer"
+import prodcontext from '../Context/ProductContext'
 function category() {
+  const context=useContext(prodcontext);
+  const {category}=context;
+  console.log(category);
   return (
     <div>
       {window.scrollTo(0, 0)}
--- src/Components/Customers/Context/ProductState.jsx
+++ src/Components/Customers/Context/ProductState.jsx
@@ -9,7 +9,9 @@ import Spinner from '../Spinner';
 
 function ProductState(props) {
     const initialproducts=[];
+    const initialcategory=[];
     const [data,setData]=useState(initialproducts);
+    const [category,setCat]=useState(initialcategory);
     let cart = [];
     
     const getProds=async ()=>{
@@ -28,19 +30,41 @@ function ProductState(props) {
       }
       catch(error)
       {
-        console.log("Error in axios");
+        console.log("Error in axios getProds");
       }
     
     }
+    const getCategory=async ()=>{
+      try{
+        const response = await axios.get("http://localhost:3000/api/customer/getCategory",{
+        headers : {
+          "Content-Type": "application/json",
+        }});
+        const responseData = await response.data;
+        //console.log(responseData);
+        setCat(responseData);
+        //console.log(data);
+      
+      
+        
+      }
+      catch(error)
+      {
+        console.log("Error in axios category");
+      }
+    
+    }
+
     useEffect(() => {
       getProds();
+      getCategory();
     }, []);
 
     if (data.length === 0) {
       return <div className="container"><Spinner/></div>; // You can display a loading indicator or any other UI while waiting for the data
     }
   return (
-    <prodcontext.Provider value={{data,getProds,cart}}>
+    <prodcontext.Provider value={{data,getProds,cart,category}}>
         {props.children }
     </prodcontext.Provider>
   )
